aec423ce64555fd4aa7fd4dba86228c9a83f0c58
Jens Sørensen <jes@interface.dk>
2015-08-19 09:31:02 +0200

    Add countries scope

--- lib/piwik/user_country.rb
+++ lib/piwik/user_country.rb
@@ -8,5 +8,23 @@ module Piwik
       getLocationFromIP
       getNumberOfDistinctCountries
     }
+
+    scoped_methods do
+      def countries params = {}
+        getCountry(defaults.merge(params))
+      end
+
+      def continents params = {}
+        getContinent(defaults.merge(params))
+      end
+
+      def regions params = {}
+        getRegion(defaults.merge(params))
+      end
+
+      def cities params = {}
+        getCity(defaults.merge(params))
+      end
+    end
   end
 end

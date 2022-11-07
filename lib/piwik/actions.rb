4f714c609ae59326b9a9195d7b112b465f2b7d27
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2022-11-07 03:32:37 +0100

    ADD: methods for sitesearch

--- lib/piwik/actions.rb
+++ lib/piwik/actions.rb
@@ -58,6 +58,14 @@ module Piwik
       def outlink(outlink_url, params = {})
         getOutlink(defaults.merge(params).merge(:outlinkUrl => outlink_url))
       end
+
+      def sitesearch_keywords params = {}
+        getSiteSearchKeywords(defaults.merge(params))
+      end
+
+      def sitesearch_no_result_keywords params = {}
+        getSiteSearchNoResultKeywords(defaults.merge(params))
+      end
     end
   end
 end
